abf9def04fa67227976f935306a0de71c734f985
Antonio Llamas <antonio.llamas@thebridge.tech>
2023-09-12 20:14:32 +0200

    feat: server side login

--- api/src/startup/routes.js
+++ api/src/startup/routes.js
@@ -17,7 +17,7 @@ module.exports = function (app) {
 	app.use(json())
 	app.use(morgan('tiny'))
 
-	// app.use('/api/users', require('../routes/users'))
+	app.use('/api/users', require('../routes/users'))
 
 	app.get('/ping', (req, res) => {
 		res.json({ status: 'success' })
